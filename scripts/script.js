0964fefaaae768bf1cc59ce90a4edd6e74cc444d
Siem.p <64197688+Siem-P@users.noreply.github.com>
2022-12-21 22:24:43 +0100

    Added Swipe progress for checklist

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,5 @@
 
-
-
-
-//  More Info handler for category cards
+//  "More Info" handler for category cards
 
 let meerInfoKnoppen = document.querySelectorAll('.card-more-info-button');
 let kaartExtraInfo = document.querySelector('.card-extra-info');
@@ -26,3 +23,33 @@ meerInfoKnoppen.forEach((meerInfoKnop) => {
 function toggleInfoHandler() {
     kaartExtraInfo.classList.toggle('hideCard');
 }
+
+// Swipe indicator progress
+
+
+const items = document.querySelectorAll('.card-container');
+const indicators = document.querySelectorAll('.swipe-indicator-item');
+
+// Observer check if element enter the viewport, and 
+// then add the active class to the element in view
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.intersectionRatio > 0) {
+        // Update the indicator elements
+        indicators.forEach((indicator, index) => {
+          if (entry.target === items[index]) {
+            indicator.classList.add('indicator-active');
+          } else {
+            indicator.classList.remove('indicator-active');	
+          }
+        });
+      }
+    });
+  });
+
+// Observe each item in the category-container 
+items.forEach(item => {
+  observer.observe(item);
+});
+
+
